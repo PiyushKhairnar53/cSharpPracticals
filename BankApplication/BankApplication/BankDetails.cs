259585c41dadf9bb6ef7846a285a4b9ddc2950f4
piyush khairnar <piyushkhairnar53@gmail.com>
2023-02-10 15:21:14 +0530

    Assg 4 Bank Application committed

---
+++ BankApplication/BankApplication/BankDetails.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    internal class BankDetails
+    {
+        public string? prmAddr = "", comAddr = "" ;
+        public bool wantCreditCard,isAddrSame;
+        public int? income,crdAllowed;
+        public string? accNo = "",crdCardNo = "";
+
+
+        public void GetBankDetails()
+        {
+            GetAddress();   
+            GenerateAccNo();
+            GetCreditDetails();
+        }
+
+        public void GetCreditDetails() 
+        {
+            Console.WriteLine("Do you want credit card (y|Y or n|N) ? : ");
+            string? cc = Console.ReadLine();
+
+            if (cc == "y" || cc == "Y" || cc=="Yes" || cc == "yes")
+            {
+                wantCreditCard = true;
+
+                if (wantCreditCard) 
+                {
+                    Console.WriteLine("Please enter your annual income : ");
+                    income = Convert.ToInt32(Console.ReadLine());
+
+                    if (income < 300000) 
+                    {
+                        Console.WriteLine("Sorry you are not eligible for Credit card");
+                    }
+                    if (income > 300000 && income <500000)
+                    {
+                        crdAllowed = 50000;
+                        Console.WriteLine("You are allowed to use credit upto : "+crdAllowed);
+                    }
+                    if (income > 500000 && income <700000)
+                    {
+                        crdAllowed = 75000;
+                        Console.WriteLine("You are allowed to use credit upto : " + crdAllowed);
+                    }
+                    if (income > 700000)
+                    {
+                        crdAllowed = 100000;
+                        Console.WriteLine("You are allowed to use credit upto : " + crdAllowed);
+                    }
+
+                }
+
+                Random rnd = new();
+                var builder = new StringBuilder();
+
+                while (builder.Length < 16)
+                {
+                    builder.Append(rnd.Next(10)).ToString();
+                }
+
+                crdCardNo = builder.ToString();
+
+
+            }
+       
+
+        }
+
+        public void GenerateAccNo() 
+        {
+            Random rnd = new();
+            var builder = new StringBuilder();
+
+            while (builder.Length < 14) 
+            {
+                builder.Append(rnd.Next(10)).ToString();
+            }
+
+            accNo = builder.ToString();
+        }
+
+        public void GetAddress()
+        {
+           
+            Console.WriteLine("Please enter your Permanant address - ");
+            prmAddr = Console.ReadLine();
+
+            Console.WriteLine("Is your permanant address and communication address same(y|Y or n|N) : ");
+            string? c = Console.ReadLine();
+
+            if (c == "y" || c == "Y" || c == "Yes" || c == "yes")
+            {
+                isAddrSame = true;
+                comAddr = prmAddr;
+            }
+            if (c == "n" || c == "N" || c == "No" || c == "no") 
+            {
+                isAddrSame = true;
+                Console.WriteLine("Please enter your Communication address - ");
+                comAddr = Console.ReadLine();
+            }
+
+        }
+
+    }
+}
---
+++ BankApplication/BankApplication/UserDetails.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BankApplication
+{
+    internal class UserDetails
+    {
+        public string? fName, lName, email, contNo, dOB, gender, spouseName, marStatus;
+        public bool haveSpouse, haveChilds;
+        public int? noOfChilds,userId=0;
+        public List<String>? childNames;
+
+        public UserDetails()
+        {
+
+            Random rnd = new();
+            userId = rnd.Next(10000,999999);    
+
+        }
+
+        public void GetDetails() {
+
+            do
+            {
+                Console.WriteLine("First name - ");
+                fName = Console.ReadLine();
+            } while (fName!.Length == 0);
+
+            do
+            {
+                Console.WriteLine("Last name - ");
+                lName = Console.ReadLine();
+            } while (lName!.Length == 0);
+
+            GetEmail();
+            GetContact();
+
+            Console.WriteLine("Enter your Date of Birth (DD/MM/YYYY) : ");
+            dOB = Console.ReadLine();
+
+            GetGender();
+            GetMaritualStatus();
+
+
+        }
+
+        public void GetEmail() {
+
+            string valid ;
+            do
+            {
+                Console.WriteLine("Please enter a valid Email id - ");
+                email = Console.ReadLine();
+                bool isEmail = Regex.IsMatch(email!, @"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z", RegexOptions.IgnoreCase);
+                valid = isEmail ? "true" : "false";
+
+            } while (valid == "false");
+
+        }
+
+        public void GetContact() {
+
+            string valid ;
+            do
+            {
+
+                Console.WriteLine("Please enter a valid Contact No : ");
+                contNo = Console.ReadLine();
+
+                bool isContNo = Regex.IsMatch(contNo!, @"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", RegexOptions.IgnoreCase);
+                valid = isContNo ? "true" : "false";
+
+            } while (valid == "false");
+
+        }
+
+        public void GetGender() {
+
+            Console.WriteLine("Enter gender (M/m or F/f): ");
+            char? g = Convert.ToChar(Console.ReadLine()!);
+            if (g == 'm' || g == 'M')
+            {
+
+                gender = "male";
+
+            }
+
+            if (g == 'f' || g == 'F')
+            {
+                gender = "female";
+            }
+
+        }
+
+        public void GetMaritualStatus() {
+
+            Console.WriteLine("Are you married (y/Y or n/N) : ");
+            string? m = Console.ReadLine();
+
+            if (m == "y" || m == "Y" || m == "Yes" || m == "yes")
+            {
+
+                marStatus = "married";
+                haveSpouse = true;
+
+                Console.WriteLine("Please enter your spouse name - ");
+                spouseName = Console.ReadLine();
+
+                Console.WriteLine("Do you have childrens (y/Y or n/N) : ");
+                string? c = Console.ReadLine();
+
+
+                if (c == "y" || c == "Y" || c == "Yes" || c == "yes")
+                {
+                    haveChilds = true;
+                    Console.WriteLine("Total no of childrens you have : ");
+                    noOfChilds = Convert.ToInt32(Console.ReadLine());
+
+                    childNames = new List<string>((int)noOfChilds);
+
+                    for (int? i = 0; i < noOfChilds; i++)
+                    {
+
+                        Console.WriteLine($"Enter Name of Child {i+1} : ");
+                        childNames.Add(Console.ReadLine()!);
+                    
+                    }
+
+                }
+
+
+            }
+
+            if (m == "n" || m == "N" || m == "No" || m == "no")
+            {
+                marStatus = "unmarried";
+                haveSpouse = false;
+            }
+
+        }
+
+
+    }
+}
